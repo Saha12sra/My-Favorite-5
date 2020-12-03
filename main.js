dc034cba79b80af4a9adea5afb92033509fb5c71
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2020-12-03 17:23:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+function welcomefav() {
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s2").style.display = "block";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function favorite() {
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s3").style.display = "block";
+    document.getElementById("s4").style.display = "block";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function hawaii() {
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s5").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function back2b() {
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s3").style.display = "block";
+    document.getElementById("s4").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function next2author() {
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s7").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function back2author() {
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s7").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function back2place() {
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("s5").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function go2ins() {
+    document.getElementById("s7").style.display = "none";
+    document.getElementById("s6").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("contactme").style.display = "none";
+}
+
+function backfromcon() {
+    document.getElementById("contactme").style.display = "none";
+    document.getElementById("s6").style.display = "block";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+}
+
+function next2con() {
+    document.getElementById("contactme").style.display = "block";
+    document.getElementById("s6").style.display = "none";
+    document.getElementById("s2").style.display = "none";
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s7").style.display = "none";
+}
